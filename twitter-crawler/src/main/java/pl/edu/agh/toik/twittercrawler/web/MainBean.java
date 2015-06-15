c1777b420e56750e30c56d7b7c3a7947aae0e4e5
Wojciech Jarosz <wojtek_j@o2.pl>
2015-06-16 00:19:22 +0200

    Finalizing project

--- twitter-crawler/src/main/java/pl/edu/agh/toik/twittercrawler/web/MainBean.java
+++ twitter-crawler/src/main/java/pl/edu/agh/toik/twittercrawler/web/MainBean.java
@@ -1,67 +1,70 @@
 package pl.edu.agh.toik.twittercrawler.web;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.Date;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.social.oauth2.OAuth2Operations;
-import org.springframework.social.oauth2.OAuth2Template;
-import org.springframework.web.client.RestTemplate;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+import org.primefaces.model.chart.PieChartModel;
+import org.springframework.transaction.annotation.Transactional;
 
-import pl.edu.agh.toik.twittercrawler.model.Tweet;
+import pl.edu.agh.toik.twittercrawler.model.Stats;
+import pl.edu.agh.toik.twittercrawler.model.Tag;
+import pl.edu.agh.toik.twittercrawler.repo.TagRepository;
 
 @ManagedBean
 @SessionScoped
 public class MainBean {
-	@Value( "${spring.social.twitter.appId}")
-	private String appId;
-	@Value( "${spring.social.twitter.appSecret}")
-	private String appSecret;
-	
-	private String appToken = fetchApplicationAccessToken(appId, appSecret);
-	
-	//private int korwinEntries = 0;
-	
-    public int getKukizEntries() {
-    	return searchTwitter("#kukiz", appToken).size();
+	public MainBean(TagRepository tagRepository){
+		this.tagRepository = tagRepository;
 	}
 	
-    public int getDudaEntries() {
-    	return searchTwitter("#duda", appToken).size();
-	}
+	TagRepository tagRepository;
+
+	private PieChartModel pieModel;
+	private LineChartModel lineChartModel;
 	
-    public int getKomorowskiEntries() {
-    	return searchTwitter("#komorowski", appToken).size();
-	}
-	
-    public int getKorwinEntries() {
-    	return searchTwitter("#korwin", appToken).size();
+    @PostConstruct
+    public void init() {
+        //createPieModel();
+    }
+
+    public LineChartModel getLineChartModel() {
+    	if (lineChartModel == null ){
+    		initLineChart();
+    	}
+    	return lineChartModel;
 	}
 
-	private static List<Tweet> searchTwitter(String query, String appToken) {
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + appToken);
-        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-        Map<String, ?> result = rest.exchange("https://api.twitter.com/1.1/search/tweets.json?q={query}", HttpMethod.GET, requestEntity, Map.class, query).getBody();
-        List<Map<String, ?>> statuses = (List<Map<String, ?>>) result.get("statuses");
-        List<Tweet> tweets = new ArrayList<Tweet>();
-        for (Map<String, ?> status : statuses) {
-            tweets.add(new Tweet(Long.valueOf(status.get("id").toString()), status.get("text").toString()));
+	private void initLineChart() {
+    	LineChartModel model = new LineChartModel();
+   	 
+        LineChartSeries series = new LineChartSeries();
+        series.setLabel("Pobrane tweety");
+        for (Stats stats : Stats.findAllStatses()){
+        	series.set(stats.getCreationDate(), stats.getRecordsInserted());
         }
-        return tweets;
-    }
-    
-    private static String fetchApplicationAccessToken(String appId, String appSecret) {
-        OAuth2Operations oauth = new OAuth2Template("jM5EyDCegfTAMk38I1ea9UkMx", "oQp9kkxIZDw0BXFis2rxtu20Bqc1NRERpp3JirMHX4jPf90n3E", "", "https://api.twitter.com/oauth2/token");
-        return oauth.authenticateClient().getAccessToken();
-    }
+        series.set(new Date(), 123);
+        series.set(new Date(), 100);
+        //model.setAnimate(true);
+        model.setLegendPosition("se");
+        model.addSeries(series);
+        lineChartModel = model;
+	}
+
+	@Transactional
+	public PieChartModel getPieModel() {
+		pieModel = new PieChartModel();
+		pieModel.setTitle("Rozklad tweetow");
+		pieModel.setLegendPosition("w");
+		pieModel.setShowDataLabels(true);
+		for (Tag tag : tagRepository.findAll()){
+			pieModel.set(tag.getContent(), tag.getTweets().size());
+		}
+		return pieModel;
+	}
 
 }
